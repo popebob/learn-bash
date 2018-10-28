46b708b5688aa6075200dc03306722a3e7a7570b
Cody Adams <cody@learningbash>
2018-10-28 15:43:53 -0400

    adding first bash scrippt.  it installs docker-ce on centos7

---
+++ install-docker/scripts/install-docker-on-centos7.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Cody Adams
+# Learning Exercise
+# 10/28/2018
+# This bash script installs docker-ce on CentOS 7
+
+YUMMY_DOCKER=https://download.docker.com/linux/centos/docker-ce.repo
+
+
+if [ "$1" == "-h" ] || [ -z "$1" ] || [ -v $YUMMY_DOCKER ]; then
+  echo "Usage: `basename $0` REQUIRED ENVARS: YUMMY_DOCKER, `basename $0` -install to install"
+  exit 0
+fi
+
+if [ "$1" == "-install" ]; then
+
+echo "Using yum repo source: " $YUMMY_DOCKER
+
+echo "Starting Pre-Install"
+
+echo "Adding yum-utils"
+
+yum install -y yum-utils \
+        device-mapper-persistent-date \
+        lvm2;
+
+echo "Installing latest docker-ce"
+
+yum install -y docker-ce
+
+echo "Installed docker-ce version:"
+
+docker version
+
+echo "Starting docker-ce" $(docker version)
+
+systemctl start docker
+
+echo "Hello, World! :)"
+
+docker run hello-world
+
+exit 0
+
+fi
