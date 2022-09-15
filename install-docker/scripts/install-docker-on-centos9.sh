7c71fb87deb47ed559f33d2b2f2210085ecb4c0d
Cody Adams <coadams@umflint.edu>
2022-09-15 00:40:43 -0400

    Update install-docker-on-centos9.sh

--- install-docker/scripts/install-docker-on-centos9.sh
+++ install-docker/scripts/install-docker-on-centos9.sh
@@ -4,7 +4,7 @@
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
     
-     sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+     sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
      
       sudo systemctl start docker
       
